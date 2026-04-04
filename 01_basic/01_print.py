01a73ebdaf44d6d119f675af78325b992fc0857d
mfernandail <maria.ibanez.leiva@gmail.com>
2026-04-03 22:45:57 -0300

    Add initial print statements for basic output demonstration

---
+++ 01_basic/01_print.py
@@ -0,0 +1,12 @@
+print("Hola mundo")
+
+print('Hola mundo con comillas simples')
+
+print("Python", "es un lenguaje de programación","muy popular")
+
+print("Hola", "con", "separador", sep = "-")
+
+print("Hola", end = " ")
+print("mundo en una sola línea")
+
+print("Hola", "mundo", sep = "-", end = "!\n") 
