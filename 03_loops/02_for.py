5469804e2b70bfeadde4e0f68409c78dc4756954
mfernandail <maria.ibanez.leiva@gmail.com>
2026-04-16 20:26:45 -0400

    Add examples of list comprehension and filtering with even numbers

--- 03_loops/02_for.py
+++ 03_loops/02_for.py
@@ -22,3 +22,15 @@ for numero in numeros:
   if(numero % 2 == 0):
     continue
   print(f"Numero: {numero}")
+
+
+
+print(f"\nList comprehension")
+animales = ["perro", "gato", "foca", "hormiga", "elefante", "huemul"]
+
+animal_upper = [animal.upper() for animal in animales]
+print(animal_upper)
+
+print("\nEjercicio")
+numPares = [num for num in [1,2,3,4,5,6] if(num % 2 == 0)]
+print(numPares)
