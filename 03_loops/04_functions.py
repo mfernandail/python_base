124fa1b35d803a8b4b00c07cd900520c2e34993a
mfernandail <maria.ibanez.leiva@gmail.com>
2026-04-22 22:34:02 -0400

    Add examples for positional, variable-length, and keyword arguments in functions

--- 03_loops/04_functions.py
+++ 03_loops/04_functions.py
@@ -41,3 +41,36 @@ print("\n-------- multiplicar --------")
 def multiplicar(numero, multiplicando = 2):
   return numero * multiplicando
 print(multiplicar(2))
+
+print("---....---")
+for _ in range(5):
+  print(_)
+
+
+print("\n-------- Parametros posicionales --------")
+def informacionPersona(nombre, edad, ciudad):
+  print(f"El nombre es: {nombre} la edad es: {edad} y la cuidad es:{ciudad}")
+
+informacionPersona(nombre="Maria", edad=37, ciudad="Arica")
+
+print("\n-------- Argumentos de longitud de variable --------")
+
+def sumarNumero(*args):
+  suma = 0
+  for numero in args:
+    suma += numero
+  return suma
+
+print(sumarNumero(1,2,3))
+print(sumarNumero(4,5))
+
+
+print("\n-------- Argumentos de clave-valor variable --------")
+
+def mostrarInformacion(**args):
+  for clave, valor in args.items():
+    print(f"{clave}: {valor}")
+print("\n")
+mostrarInformacion(nombre="Maria", edad=37)
+print("\n")
+mostrarInformacion(ciudad="Arica", pais="Chile", codigo=10000)
