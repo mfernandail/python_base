41799fad066785aae68e76aedff8450d7142f7c3
mfernandail <maria.ibanez.leiva@gmail.com>
2026-04-25 23:13:06 -0400

    Add improved function to check equilibrium between Reed Richards and Johnny Storm, and implement a function to sum even numbers from a list of dinosaur eggs

--- 04_logic/01_desafio.py
+++ 04_logic/01_desafio.py
@@ -15,6 +15,8 @@ Crea una función en Python que reciba una cadena de texto. Esta función debe c
 - En el caso de que no aparezca ninguna de las dos letras en la cadena, se entiende que el equilibrio se mantiene (0 = 0), por lo que la función debe retornar True.
 """
 
+# Mi version
+
 def fantastic_four(cadena):
   count_j = 0
   count_r = 0
@@ -37,3 +39,40 @@ def fantastic_four(cadena):
 
 resultado = fantastic_four("Rrr JjJ hola mundo jaja")
 print(f"Resultado: {resultado}")
+
+# Mejorada
+print("\n ----------------")
+print("\n ----------------")
+
+def fantastic_four_two(cadena):
+  cadena = cadena.lower()
+  count_j = cadena.count("j")
+  count_r = cadena.count("r")
+
+  print(f"J = {count_j} - R = {count_r}")
+  return count_j == count_r
+
+print(fantastic_four_two("JjJJj RrrRr"))
+
+"""
+En Jurassic Park, se ha observado que los dinosaurios carnívoros, como el temible T-Rex, depositan un número par de huevos. Imagina que tienes una lista de números enteros en la que cada número representa la cantidad de huevos puestos por un dinosaurio en el parque.
+
+Importante: Solo se consideran los huevos de los dinosaurios carnívoros (T-Rex) aquellos números que son pares.
+
+Objetivo:
+Escribe una función en Python que reciba una lista de números enteros y devuelva la suma total de los huevos que pertenecen a los dinosaurios carnívoros (es decir, la suma de todos los números pares en la lista).
+"""
+print("\n ----------------")
+print("\n ----------------")
+
+huevos = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def h_carnivoros(huevos_din):
+  suma = 0
+
+  for huevo_din in huevos_din:
+    if huevo_din % 2 == 0:
+      suma += huevo_din
+  return suma
+
+print(h_carnivoros(huevos))
