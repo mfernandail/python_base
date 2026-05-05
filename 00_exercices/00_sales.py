1c249c4741bfc7038306200637a4a065ac1affcc
mfernandail <maria.ibanez.leiva@gmail.com>
2026-05-04 21:23:20 -0400

    Add initial implementation of a shopping list program with menu options

---
+++ 00_exercices/00_sales.py
@@ -0,0 +1,29 @@
+# Crea una lista vacía llamada "compras"
+
+compras = []
+
+# Inicia un ciclo infinito (while True)
+
+    # Muestra un menú:
+    # 1. Agregar producto
+    # 2. Ver lista
+    # 3. Salir
+
+    # Pide al usuario una opción
+
+    # Si la opción es 1:
+        # Pide el nombre de un producto
+        # Agrega el producto a la lista
+
+    # Si la opción es 2:
+        # Verifica si la lista está vacía
+            # Si está vacía, muestra "Lista vacía"
+            # Si no:
+                # Recorre la lista
+                # Muestra cada producto
+
+    # Si la opción es 3:
+        # Termina el programa (break)
+
+    # Si la opción no es válida:
+        # Muestra mensaje de error
