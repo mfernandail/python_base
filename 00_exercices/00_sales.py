64c464b5035b70448456069224b7b5a3dbeac3a3
mfernandail <maria.ibanez.leiva@gmail.com>
2026-05-11 20:42:06 -0400

    Refactor shopping list functionality to improve user input handling and add product editing feature

--- 00_exercices/00_sales.py
+++ 00_exercices/00_sales.py
@@ -1,25 +1,38 @@
-# Crea una lista vacía llamada "compras"
-
 compras = []
 
-# Inicia un ciclo infinito (while True)
+def pedir_numero(mensaje):
+  while True:
+    try:
+      numero = int(input(mensaje))
+      return numero
+    except:
+      print("Solo se admiten números")
+      
+
 while True:
   print("\n")
   print("1. Agregar producto")
   print("2. Ver lista")
   print("3. Salir")
   print("4. Eliminar producto")
+  print("5. Editar producto")
   
-  respuesta = int(input("\n¿Cual opcion eliges? "))
-
+  respuesta = pedir_numero("\n¿Cual opcion eliges? ")
+  
   if respuesta == 1:
     nombre_producto = input("Ingresa el nombre de un producto: ")
-    compras.append(nombre_producto)
+    cantidad_producto = pedir_numero("Ingresa la cantidad: ")
+    
+    compras.append({
+      "nombre": nombre_producto,
+      "cantidad": cantidad_producto
+    })
     
   elif respuesta == 2:
     if len(compras) == 0:
       print("Lista vacía")
     else:
+      print("\n")
       for i, compra in enumerate(compras):
         print(f"Compras #{i + 1}: {compra}")
     
@@ -32,31 +45,30 @@ while True:
     for i, compra in enumerate(compras):
       print(f"Id: {i} ({compra})")
     
-    id_eliminar = int(input("Ingrese el id: "))
+    id_eliminar = pedir_numero("Ingrese el id: ")
     
-    if id_eliminar > 0 & id_eliminar < len(compras):
+    if id_eliminar >= 0 and id_eliminar < len(compras):
       compras.pop(id_eliminar)
     else:
       print(f"El id: {id_eliminar} no existe")
+  
+  elif respuesta == 5:
+    nombre_producto_buscar = input("¿Qué producto quieres editar? ")
+
+    encontrado = False
     
+    for compra in compras:
+      if compra["nombre"] == nombre_producto_buscar:
+        encontrado = True
+    
+        print(f"Producto encontrado: {nombre_producto_buscar} - cantidad: {compra["cantidad"]}")    
+        nueva_cantidad = pedir_numero(f"Ingresa la nueva cantidad para {nombre_producto_buscar}: ")
+        
+        compra["cantidad"] = nueva_cantidad
+        break
+
+    if not encontrado:
+      print(f"Producto: {nombre_producto_buscar} no encontrado")
+  
   else:
     print("opción no es válida")  
-
-    # Pide al usuario una opción
-
-    # Si la opción es 1:
-        # Pide el nombre de un producto
-        # Agrega el producto a la lista
-
-    # Si la opción es 2:
-        # Verifica si la lista está vacía
-            # Si está vacía, muestra "Lista vacía"
-            # Si no:
-                # Recorre la lista
-                # Muestra cada producto
-
-    # Si la opción es 3:
-        # Termina el programa (break)
-
-    # Si la opción no es válida:1
-        # Muestra mensaje de error
