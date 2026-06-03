778a5da09fb50aa462896f88230a44065f9b8563
mfernandail <maria.ibanez.leiva@gmail.com>
2026-06-02 21:03:46 -0400

    Refactor calculator functions to improve input validation and streamline operation handling

--- 00_exercices/02_calculadora.py
+++ 00_exercices/02_calculadora.py
@@ -1,11 +1,15 @@
-def validar_numero(input_ingresado, num_min = 0):
+operaciones = {
+  1: "+",
+  2: "-",
+  3: "*",
+  4: "/"
+}
+
+def validar_numero(input_ingresado):
   while True:
     try:
       numero = int(input(input_ingresado))
-      if numero >= num_min:
-        return numero
-      else:
-        print(f"\nEl número debe ser mayor o igual a {num_min}")
+      return numero
     except ValueError:
       print("\nSolo se admiten números")
 
@@ -17,10 +21,39 @@ def ver_acciones():
   print("4. Dividir")
   print("5. Salir")
 
+def pedir_numeros():
+  numero_1 = validar_numero("Primer número: ")
+  numero_2 = validar_numero("Segundo número: ")
+
+  return numero_1, numero_2
+
+def operacion(op):
+  n1, n2 = pedir_numeros()
+
+  if op == "+":
+    resultado = n1 + n2
+  elif op == "-":
+    resultado = n1 - n2
+  elif op == "*":
+    resultado = n1 * n2
+  elif op == "/":
+    if n2 == 0:
+      print(f"El divisor debe ser diferente a 0")
+      return
+
+    resultado = n1 / n2
+  
+  print(f"{n1} {op} {n2} = {resultado}")
 
 while True:
   ver_acciones()
 
-  operacion_seleccionada = validar_numero("Selecciona una operacion: ", 1)
+  operacion_seleccionada = validar_numero("Selecciona una operacion: ")
 
-  print(f"Seleccionaste: {operacion_seleccionada}")
+  if operacion_seleccionada < 1 or operacion_seleccionada > 5:
+    print("Número fuera de rango")
+    continue
+  if operacion_seleccionada == 5:
+    break
+  else:
+    operacion(operaciones[operacion_seleccionada])
