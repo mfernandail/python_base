52e4d157d4ad279ab4617a8288d4949506c9b817
mfernandail <maria.ibanez.leiva@gmail.com>
2026-06-14 20:46:09 -0400

    Add eliminar_gasto function and update options for expense management

--- 00_exercices/03_gestor_gastos.py
+++ 00_exercices/03_gestor_gastos.py
@@ -5,7 +5,8 @@ opciones = {
   2: "Ver todos los gastos",
   3: "Ver total gastado",
   4: "Ver gastos por categoría",
-  5: "Salir"
+  5: "Eliminar gasto",
+  6: "Salir"
 }
 
 try:
@@ -18,15 +19,18 @@ def guardar_json():
   with open("./00_exercices/json/gastos_operaciones.json", "w") as items:
     json.dump(gastos, items, indent=2)
 
-def validar_numero(input_ingresado, num_min = 0):
+def validar_numero(input_ingresado, num_min = 0, num_max = None):
   while True:
     try:
       numero = int(input((input_ingresado)))
       
-      if numero >= num_min:
-        return numero
-      else:
-        print("Debe ser número ser mayor")
+      if numero < num_min:
+        print(f"Debe ser mayor o igual a {num_min}")
+        continue
+      if numero is not None and numero > num_max:
+        print(f"Debe ser menor o igual a {num_max}")
+        continue
+      return numero
 
     except ValueError:
       print("Debe ser número entero")
@@ -61,7 +65,7 @@ def agregar_gasto():
   print(gastos)
 
 def ver_gastos():
-  if len(gastos) == 0:
+  if not gastos:
     print("No hay gastos para mostrar")
     return
   
@@ -73,7 +77,7 @@ def ver_gastos():
   print(f"\nTotal: ${total_gastos}")
 
 def ver_gastos_total():
-  if len(gastos) == 0:
+  if not gastos:
     print("No hay gastos para mostrar")
     return
   
@@ -82,7 +86,7 @@ def ver_gastos_total():
   print(f"\nTotal: ${total_gastos}")
 
 def ver_gastos_categoria():
-  if len(gastos) == 0:
+  if not gastos:
     print("No hay gastos para mostrar")
     return
   
@@ -98,6 +102,21 @@ def ver_gastos_categoria():
 
     
   print(gasto_categorias)
+
+def eliminar_gasto():
+  if not gastos:
+    print("No hay gastos para mostrar")
+    return
+  
+  for i, gasto in enumerate(gastos):
+    print(f"{i + 1}. {gasto['descripcion']} - ${gasto['monto']} - {gasto['categoria']}")
+  
+  gasto_eliminar = validar_numero("Ingresa el numero del gasto que quieres eliminar: ", 1, len(gastos))
+
+  gastos.pop(gasto_eliminar - 1)
+  guardar_json()
+
+
 
 def ver_opciones():
   print("\n\n===== OPCIONES =====")
@@ -117,6 +136,7 @@ while True:
   print(f"Opción seleccionada: {opciones[opcion_ingresada]}")
   print("= = = = = = = = = = = = = = = = = = = = = = = =\n")
 
+
   if opcion_ingresada == 1:
     agregar_gasto()
   elif opcion_ingresada == 2:
@@ -126,4 +146,6 @@ while True:
   elif opcion_ingresada == 4:
     ver_gastos_categoria()
   elif opcion_ingresada == 5:
+    eliminar_gasto()
+  elif opcion_ingresada == 6:
     break
